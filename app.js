19f349e972cedc178d3b5415057550464b4b10dc
Lukas <54035973+LukasBaranowski@users.noreply.github.com>
2020-07-04 15:40:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+
+gulp.task("default", function () {
+  return gulp.src("src/app.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+});
+
 function hello() {
     document.write("Hello KRKdev")
 };
